2beba7295602398bc7453035710589c93d4edb07
Rohan <rp331563@gmail.com>
2023-09-26 22:53:39 +0530

    loginpage

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import Sidebar from "./components/views/Sidebar";
 import Feed from "./components/views/Feed";
 import Widgets from "./components/views/Widgets";
+import Auth from "./components/views/Auth";
 
 function App() {
-  return (
-    //BEM convention
-    <div className="App">
-       
-     {/*Sidebar */}
-       <Sidebar/>
+  const auth = false
+  if (auth){
+    return (
+      //BEM convention
+      <div className="App">
+       {/*Sidebar */}
+         <Sidebar/>
+  
+         {/*Feed */}
+         <Feed/>
+  
+          {/*Widgets*/}
+          <Widgets/>
+         </div>
+    );
+  }
+  else{
+    return(
+      <Auth/>
+    )
+  }
 
-       {/*Feed */}
-       <Feed/>
 
-        {/*Widgets*/}
-        <Widgets/>
-       </div>
-  );
+  
 }
 
 export default App;
